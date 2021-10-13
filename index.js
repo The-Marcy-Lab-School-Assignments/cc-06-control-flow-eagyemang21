6561acb93de0d6fa3dd796e843d503bdc0c203c9
eagyemang21 <elijahagyemang9@gmail.com>
2021-10-13 23:30:24 +0000

    code challenge

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
 //1
 function sumOfThreeAndFive(){
-  
+ Let sum = 0
+for(let i = 1; i <= 1000; i++){
+if (i % 3 === 0 && i % 5 === 0){
+Sum += i
+  }
+ } return sum 
 }
+console.log(sumOfThreeAndFive())
 
-function greaterNum(){
-  
+function greaterNum(num1, num2){
+ if(num1 === num2){
+Return “both integers are equal”
+}else if(num1 > num2){
+Return num1
+}else if(num2 > num1){
+Return num2
+}else{
+Return null
 }
+console.log(greaterNum(4, 6))
 
 function oddAndEvenInFifteen(){
   
